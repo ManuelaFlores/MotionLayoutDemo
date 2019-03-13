21eb6adf439f1cdf605544e3629242bf00b72063
Manuela Flores <fmanuela499@gmail.com>
2019-03-13 04:41:03 -0500

    example implementation :art:

--- app/src/main/java/com/manuflowers/motionlayoutdemo/MainActivity.kt
+++ app/src/main/java/com/manuflowers/motionlayoutdemo/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.motion_05_imagefilter)
+        setContentView(R.layout.motion_01_basic)
 
 
 
